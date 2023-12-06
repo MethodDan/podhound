a7e2accae334e1ff535ea4c2a8f30f42716c0a48
Dan Lynch <dan@danlynch.org>
2023-12-06 15:10:21 +0000

    Trying to fix admin area to show models

--- hound/admin.py
+++ hound/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Publisher, Episode, Show
+
+class ShowAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Show)
+admin.site.register(Episode)
